2150c216299131661c688dd9d8fde1466f7162c7
SuperIden3 <gomesshely0@gmail.com>
2022-02-18 14:21:25 -0500

    Create pickRandom().js

---
+++ pickRandom().js
@@ -0,0 +1,8 @@
+/*
+  visit https://www.delftstack.com/howto/javascript/javascript-pick-random-from-array for more info.
+  Website not mine btw.
+*/
+var pickRandom = function(arrayOrObject) {
+  var random = Math.random() * array.length | 0;
+  return arrayOrObject[random]
+}
