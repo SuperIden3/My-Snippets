ce1c4a0308622501c07d3e7705f6912dba2d8efc
SuperIden3 <gomesshely0@gmail.com>
2022-02-23 14:50:37 -0500

    Create Greet().js

---
+++ Greet().js
@@ -0,0 +1,15 @@
+class greet {
+  constructor(message) {
+    this.message = message;
+  }
+  say() {
+    console.log(`${this.message}`
+  }
+}
+
+/*
+  Input:
+    var hola = new greet('Hola!');
+  Output:
+    Hola!
+*/
