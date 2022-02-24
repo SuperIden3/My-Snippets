42c0c8ba484928ab1a429446b34a07d6c3532f5a
SuperIden3 <gomesshely0@gmail.com>
2022-02-23 19:22:48 -0500

    Create My First class Declaration.js
    This is the first time I declared a class in JavaScript, and it works.
    I just need to name it as a variable and not a class name.

---
+++ My First class Declaration.js
@@ -0,0 +1,9 @@
+var person = class {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+}
+/*
+  var [person] = new person([firstname], [lastname])
+*/
