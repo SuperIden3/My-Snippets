11a3f28fb1f15288724415486ca68023f5a83983
SuperIden3 <gomesshely0@gmail.com>
2022-04-03 16:31:29 -0400

    Average in Java
    I got this from GeeksforGeeks. Website isn't mine, btw.

--- Average.java
+++ Average.java
@@ -1,3 +1,17 @@
 /*
   Got this idea from https://www.geeksforgeeks.org/scanner-class-in-java/, with a few modifications.
 */
+
+import java.util.Scanner;
+public class Program {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        float sum = 0, count = 0;
+        while (input.hasNextFloat()){
+            float num = input.nextFloat();
+            sum += num;
+            count++;
+        }
+        System.out.println("Average: " + sum / count);
+    }
+}
