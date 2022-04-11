56edfec042ae65467e1408308a176c42ecc9a9ef
SuperIden3 <gomesshely0@gmail.com>
2022-04-11 13:52:34 -0400

    Update pickRandom Function.js

--- pickRandom Function.js
+++ pickRandom Function.js
@@ -1,5 +1,6 @@
 /*
   visit https://www.delftstack.com/howto/javascript/javascript-pick-random-from-array for more info.
+  Credit to that person entirely.
   Website not mine btw.
 */
 var pickRandom = function(array) {
