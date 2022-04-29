12fa100a279d6cf30204ba6310f67a1bc908b8f8
SuperIden3 <gomesshely0@gmail.com>
2022-04-29 09:30:48 -0400

    Create New Integer and Float with Inheritance from Number.js
    Make a new integer and float with inheritance from the `Number` class.

---
+++ New Integer and Float with Inheritance from Number.js
@@ -0,0 +1,12 @@
+var Integer = class {
+  constructor(num) {
+    num = parseInt(super(num));
+    this.num = num;
+  }
+}
+var Float = class {
+  constructor(num) {
+    num = parseFloat(super(num));
+    this.num = num;
+  }
+}
