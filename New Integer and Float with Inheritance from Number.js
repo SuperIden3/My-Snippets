9b53d45e8c005f6db8c1bd7be131b3b6297a770a
SuperIden3 <gomesshely0@gmail.com>
2022-05-02 14:24:42 -0400

    Update New Integer and Float with Inheritance from Number.js

--- New Integer and Float with Inheritance from Number.js
+++ New Integer and Float with Inheritance from Number.js
@@ -3,10 +3,16 @@ var Integer = class extends Number {
     num = parseInt(super(num));
     this.num = num;
   }
+  number() {
+    return this.num;
+  }
 }
 var Float = class extends Number {
   constructor(num) {
     num = parseFloat(super(num));
     this.num = num;
+  }
+  number() {
+    return this.num;
   }
 }
