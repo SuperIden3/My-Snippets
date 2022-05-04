eafb8073bcc532494a74efaecd3f7ff750016f0d
SuperIden3 <gomesshely0@gmail.com>
2022-05-04 10:22:31 -0400

    Create My Own Bare isArray Function (1.0.1).js

---
+++ My Own Bare isArray Function (1.0.1).js
@@ -0,0 +1,7 @@
+const isArray = function(variable) {
+    if(!!variable && typeof variable == "object" && (!!variable[0] || !!variable[1] || !!variable[2]) {
+        return true;
+    } else {
+        return false;
+    }
+}
