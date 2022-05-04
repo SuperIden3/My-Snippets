689a8048b6bedbfe5e1abf218f1cf7f0a3fa474f
SuperIden3 <gomesshely0@gmail.com>
2022-05-04 10:28:56 -0400

    Delete My Own Bare isArray Function (1.0.1).js

--- My Own Bare isArray Function (1.0.1).js
+++
@@ -1,7 +0,0 @@
-const isArray = function(variable) {
-    if(!!variable && typeof variable == "object" && (!!variable[0] || !!variable[1] || !!variable[2]) {
-        return true;
-    } else {
-        return false;
-    }
-}
