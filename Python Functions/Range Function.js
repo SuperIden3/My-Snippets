f1919bc692f2438a0aed2400a00d2b11e5fc8019
SuperIden3 <gomesshely0@gmail.com>
2022-05-09 10:51:51 -0400

    Update Range Function.js

--- Python Functions/Range Function.js
+++ Python Functions/Range Function.js
@@ -1,7 +1,35 @@
-const range = (start, end) => {
-    let array = [];
-    for (let i = start; i <= end; i++) {
-        array.push(i);
-    }
-	  return array;
-};
+var range = {
+	/**
+	 * @param {number} start The starting number
+	 * @param {number} end The ending number
+	 * @returns {any} array The array produced and returned from the function
+	 */
+	forward: (start, end) => {
+
+		if (start != end) {
+			let array = [];
+			for (let i = start; i <= end; i++) {
+				array.push(i);
+			}
+			return array;
+		} else {
+			throw new RangeError("First argument cannot be the same as second argument. You can use `[range([first], [first + 1]).pop()]`");
+		}
+	},
+	/**
+	 * @param {number} start The starting number
+	 * @param {number} end The ending number
+	 * @returns {any} array The array produced and returned from the function
+	 */
+	backward: (start, end) => {
+		if (start != end) {
+			let array = [];
+			for (let i = start; i >= end; i--) {
+				array.push(i);
+			}
+			return array;
+		} else {
+			throw new RangeError("First argument cannot be the same as second argument. You can use `[range([first], [first + 1]).pop()]`");
+		}
+	}
+}
