291c2546c87514733e3b6b4c5972ae91c14712bb
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-05-24 11:06:13 -0400

    Update Tuple.js

--- Python Functions/Tuple.js
+++ Python Functions/Tuple.js
@@ -1,7 +1,10 @@
 class Tuple {
   constructor(...items) {
     this.items = items
-    this.length = this.items.length
+    this.length = {
+        count: this.items.length,
+        "index count": this.items.length - 1
+    }
     Object.freeze(this.items)
   }
 }
