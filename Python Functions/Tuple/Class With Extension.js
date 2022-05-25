20e6992de0483ab669934fab476577eb55d5411d
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-05-25 11:24:20 -0400

    Update Class With Extension.js

--- Python Functions/Tuple/Class With Extension.js
+++ Python Functions/Tuple/Class With Extension.js
@@ -1,8 +1,6 @@
 class Tuple extends Array {
   constructor(...items) {
     super(items)
-    this.items = items
-    this.length = new Map([["count", this.items.length], ["index count", this.items.length - 1]])
-    Object.freeze(this.items)
+    Object.freeze(super(items))
   }
 }
