550766ce2e6b30ec5c2bc6f58f95093e922ffff4
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-05-26 08:50:07 -0400

    Update Class without Extension.js

--- Python Functions/Tuple/Class without Extension.js
+++ Python Functions/Tuple/Class without Extension.js
@@ -1,10 +1,6 @@
 class Tuple {
   constructor(...items) {
     this.items = items
-    this.length = {
-        count: this.items.length,
-        "index count": this.items.length - 1
-    }
     Object.freeze(this.items)
   }
 }
