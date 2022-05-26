fbd3a6d94b255d2dfa8ba23fdb5d632dd0f261d7
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-05-26 08:49:50 -0400

    Update With the 0.js

--- Python Functions/Tuple/Class with Extension/With the 0.js
+++ Python Functions/Tuple/Class with Extension/With the 0.js
@@ -2,9 +2,7 @@ class Tuple extends Array {
   constructor(...items) {
     super(items)
     this.items = this[0]
-    this[0] = undefined
-    this.length = this["items"].length
     Object.freeze(this.items)
-    Object.freeze(this.length)
+    Object.freeze(this[0])
   }
 }
