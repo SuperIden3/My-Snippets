f18e6f01caf43ff8a2f71d95487c0533490798d2
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-06-03 08:04:59 -0400

    Update Lambda Function.js

--- Python Functions/Lambda Function.js
+++ Python Functions/Lambda Function.js
@@ -2,6 +2,6 @@ var lambda = (equation,...vars) => {
     return eval(equation)
 }
 lambda.manual = {
-    "how it works": "As long as you declare the variables, in sloppy mode/nom-strict mode, the function will work",
+    "how it works": "As long as you declare the variables, in sloppy mode/non-strict mode, the function will work",
     "warning!": "This might not work in strict mode!"
 }
