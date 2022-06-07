e8021585a033f06a4b72f122dce41250da672b6e
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-06-07 09:42:22 -0400

    Update 2.3.0.js

--- Python Functions/Range Function/2.3.0.js
+++ Python Functions/Range Function/2.3.0.js
@@ -13,15 +13,14 @@ const range = (start,end,step = 1) => {
         return array
     } else {
         if(start === end) {
-          throw new RangeError("Arguments are the same.")
+          return new RangeError("Arguments are the same.")
         } else if(typeof start !== "number" || typeof end !== "number") {
-          throw new RangeError("Arguments are not type of number.")
+          return new RangeError("Arguments are not type of number.")
         } else if(start === Infinity || end === Infinity || step === Infinity) {
-          throw new RangeError("Arguments are Infinity.")
+          return new RangeError("Arguments are Infinity.")
         } else {
-          throw new Error(undefined)
+          return new Error(undefined)
         }
-        return undefined
     }
 }
 // Arguments must be type of number, not equal to each other, and must not equal to Infinity, for preventing a crash.
