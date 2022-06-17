069f9b3bb3cdd732af1365ad3cddc9226efa361f
SuperIden3 <95597246+SuperIden3@users.noreply.github.com>
2022-06-17 08:23:46 -0400

    Create Loading and Waiting.js

---
+++ Loading and Waiting.js
@@ -0,0 +1,19 @@
+console.log("Loading...")
+setTimeout(
+  () => {
+    console.log("Successfully loaded")
+  },
+  4000
+)
+setTimeout(
+  () => {
+    console.log("Waiting...")
+  },
+  5000
+)
+setTimeout(
+  () => {
+    console.log("All done")
+  },
+  7000
+)
